fa63003a1430c554d754286598bbbcd05629c000
Chrispy21 <chrispy.21@hotmail.com>
2017-10-08 17:26:46 +0200

    Alap játék kidolgozása, menü hozzáadása

---
+++ Mario/Game1.cs
@@ -0,0 +1,161 @@
+﻿using Mario.States;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Mario
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        public GraphicsDeviceManager window;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D character;
+
+        private State _currentState;
+
+        private State _nextState;
+
+        int characterX;
+        int characterY;
+
+        public void ChangeState(State state)
+        {
+            _nextState = state;
+        }
+
+        public void GameWindow()
+        {
+            window.PreferredBackBufferWidth = 800;
+            window.PreferredBackBufferHeight = 600;
+            window.ApplyChanges();
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+
+            GameWindow();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            character = Content.Load<Texture2D>("Others/character");
+
+            _currentState = new MenuState(this, graphics.GraphicsDevice, Content);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (_nextState != null)
+            {
+                _currentState = _nextState;
+
+                _nextState = null;
+            }
+
+            _currentState.Update(gameTime);
+
+            _currentState.PostUpdate(gameTime);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                characterX -= 10;
+                
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                characterX += 10;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                characterY -= 10;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                characterY += 10;
+            }
+
+            if (characterX < 0)
+            {
+                characterX = 0;
+            }
+            else if (characterX + character.Width > graphics.PreferredBackBufferWidth)
+            {
+                characterX = graphics.PreferredBackBufferWidth - character.Width;
+            }
+            if (characterY < 0)
+            {
+                characterY = 0;
+            }
+            else if (characterY + character.Height > graphics.PreferredBackBufferHeight)
+            {
+                characterY = graphics.PreferredBackBufferHeight - character.Height;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _currentState.Draw(gameTime, spriteBatch);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(character, new Vector2(characterX, characterY), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Mario/States/MenuState.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Mario.Controls;
+
+namespace Mario.States
+{
+    public class MenuState : State
+    {
+        private List<Component> _components;
+
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base (game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("Controls/Button");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+
+            var newGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 200),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += newGameButton_Click;
+
+            var loadGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 250),
+                Text = "Load Game",
+            };
+
+            loadGameButton.Click += loadGameButton_Click;
+
+            var quitGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 300),
+                Text = "Quit Game",
+            };
+
+            quitGameButton.Click += quitGameButton_Click;
+
+            _components = new List<Component>()
+            {
+                newGameButton,
+                loadGameButton,
+                quitGameButton,
+            };
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+
+            
+
+            spriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+                component.Update(gameTime);
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            //Sprite-ok eltüntetése, ha nem kell nekünk
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new GameState(_game, _graphicsDevice, _content)); 
+        }
+
+        private void loadGameButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Load Game");
+        }
+
+        private void quitGameButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
+    }
+}
